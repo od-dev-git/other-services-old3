cd4b6086c3a3beae73d52e98a50cf3df869e7bc9
Prasun Naiya <prasun.naiya@pwc.com>
2022-07-25 11:33:34 +0530

    report services build issue fix

--- report-services/src/main/java/org/egov/report/ReportServicesApplication.java
+++ report-services/src/main/java/org/egov/report/ReportServicesApplication.java
@@ -1,11 +1,19 @@
 package org.egov.report;
 
+import java.util.TimeZone;
+
 import org.egov.tracer.config.TracerConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "org.egov.report" })
@@ -15,5 +23,14 @@ public class ReportServicesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReportServicesApplication.class, args);
+	}
+	
+	@Value("${app.timezone}")
+    private String timeZone;
+	
+	@Bean
+	public ObjectMapper objectMapper() {
+		return new ObjectMapper().configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
+				.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES).setTimeZone(TimeZone.getTimeZone(timeZone));
 	}
 }
