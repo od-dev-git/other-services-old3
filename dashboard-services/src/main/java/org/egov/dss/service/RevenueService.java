6b22d9d8c4e7babb8185254f17ee686f6e53f18c
Mohan Sharma <mohan.sharma@pwc.com>
2022-09-16 07:27:05 +0000

    Impl added for todaysColl,UlbPerf and mutationColl

--- dashboard-services/src/main/java/org/egov/dss/service/RevenueService.java
+++ dashboard-services/src/main/java/org/egov/dss/service/RevenueService.java
@@ -2,19 +2,30 @@ package org.egov.dss.service;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.stream.Collectors;
 
+import org.apache.catalina.mapper.Mapper;
 import org.egov.dss.model.Payment;
 import org.egov.dss.model.PaymentSearchCriteria;
 import org.egov.dss.model.enums.PaymentStatusEnum;
 import org.egov.dss.repository.PaymentRepository;
+import org.egov.dss.util.DashboardUtils;
 import org.egov.dss.web.model.ChartCriteria;
 import org.egov.dss.web.model.Data;
+import org.egov.dss.web.model.Plot;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
+import com.fasterxml.jackson.annotation.JacksonInject.Value;
 import com.google.common.collect.Sets;
 
 @Service
@@ -22,6 +33,9 @@ public class RevenueService {
 	
 	@Autowired
 	private PaymentRepository paymentRepository;
+	
+	@Autowired
+	private DashboardUtils dashboardUtils;
 	
 	private PaymentSearchCriteria getPaymentSearchCriteria(ChartCriteria chartCriteria) {
 		PaymentSearchCriteria criteria = new PaymentSearchCriteria();
@@ -58,8 +72,17 @@ public class RevenueService {
 	}
 
 	public List<Data> todaysCollection(ChartCriteria chartCriteria) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		PaymentSearchCriteria paymentSearchCriteria = getPaymentSearchCriteria(chartCriteria);
+		List<Payment> payments = paymentRepository.getPayments(paymentSearchCriteria);
+		BigDecimal todaysCollection = payments.parallelStream()
+				.filter(pay -> pay.getPaymentStatus() != PaymentStatusEnum.CANCELLED)
+				.filter(pay -> !pay.getTenantId().equalsIgnoreCase("od.testing"))
+				.filter(pay -> pay.getTransactionDate() >= dashboardUtils.getStartingTime(System.currentTimeMillis()))
+				.filter(pay -> pay.getTransactionDate() <= dashboardUtils.getEndingTime(System.currentTimeMillis()))
+				.map(pay -> pay.getTotalAmountPaid()).reduce(BigDecimal.ZERO, BigDecimal::add);
+		
+		return Arrays.asList(Data.builder().headerValue(todaysCollection.setScale(2, RoundingMode.HALF_UP)).build());
 	}
 
 	public List<Data> targetCollection(ChartCriteria chartCriteria) {
@@ -73,8 +96,17 @@ public class RevenueService {
 	}
 
 	public List<Data> totalMutationFeeCollection(ChartCriteria chartCriteria) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		PaymentSearchCriteria paymentSearchCriteria = getPaymentSearchCriteria(chartCriteria);
+		paymentSearchCriteria.setBusinessServices(Sets.newHashSet("PT.MUTATION"));
+		List<Payment> payments = paymentRepository.getPayments(paymentSearchCriteria);
+		BigDecimal totalCollection = payments.parallelStream()
+				.filter(pay -> pay.getPaymentStatus() != PaymentStatusEnum.CANCELLED)
+				.filter(pay -> !pay.getTenantId().equalsIgnoreCase("od.testing"))
+				.filter(pay -> pay.getPaymentDetails().get(0).getBusinessService().equalsIgnoreCase("PT.MUTATION"))
+				.map(pay -> pay.getTotalAmountPaid()).reduce(BigDecimal.ZERO, BigDecimal::add);
+		
+		return Arrays.asList(Data.builder().headerValue(totalCollection.setScale(2, RoundingMode.HALF_UP)).build());
 	}
 
 	public List<Data> cumulativeCollection(ChartCriteria chartCriteria) {
@@ -83,13 +115,64 @@ public class RevenueService {
 	}
 
 	public List<Data> topPerformingUlbs(ChartCriteria chartCriteria) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		PaymentSearchCriteria paymentSearchCriteria = getPaymentSearchCriteria(chartCriteria);
+		List<Payment> payments = paymentRepository.getPayments(paymentSearchCriteria);
+		Map<String, List<Payment>> tenantWisePayments = payments.parallelStream()
+				.filter(pay -> pay.getPaymentStatus() != PaymentStatusEnum.CANCELLED)
+				.filter(pay -> !pay.getTenantId().equalsIgnoreCase("od.testing"))
+				.collect(Collectors.groupingBy(Payment::getTenantId));
+		
+		HashMap<String, BigDecimal> tenantWiseAmountCollection = new HashMap<>();
+		tenantWisePayments.forEach((key,value) -> {
+			BigDecimal sum = value.stream().collect(Collectors.mapping(Payment::getTotalAmountPaid, Collectors.reducing(BigDecimal.ZERO, BigDecimal::add)));
+			tenantWiseAmountCollection.put(key, sum);
+		});
+		
+		List<Data> responseList = new ArrayList<>();
+		
+		Map<String, BigDecimal> tenantWiseAmountCollectionSorted = tenantWiseAmountCollection.entrySet().parallelStream()
+        .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+		
+		tenantWiseAmountCollectionSorted.forEach((key,value) -> {
+			List<Plot> plots = Arrays.asList(Plot.builder().name(key).value(value).build());
+			responseList.add(Data.builder().plots(plots).build());
+		});
+		
+		return responseList;
 	}
 
 	public List<Data> bottomPerformingUlbs(ChartCriteria chartCriteria) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		PaymentSearchCriteria paymentSearchCriteria = getPaymentSearchCriteria(chartCriteria);
+		List<Payment> payments = paymentRepository.getPayments(paymentSearchCriteria);
+		Map<String, List<Payment>> tenantWisePayments = payments.parallelStream()
+				.filter(pay -> pay.getPaymentStatus() != PaymentStatusEnum.CANCELLED)
+				.filter(pay -> !pay.getTenantId().equalsIgnoreCase("od.testing"))
+				.collect(Collectors.groupingBy(Payment::getTenantId));
+		
+		HashMap<String, BigDecimal> tenantWiseAmountCollection = new HashMap<>();
+		tenantWisePayments.forEach((key,value) -> {
+			BigDecimal sum = value.stream().collect(Collectors.mapping(Payment::getTotalAmountPaid, Collectors.reducing(BigDecimal.ZERO, BigDecimal::add)));
+			tenantWiseAmountCollection.put(key, sum);
+		});
+		
+		List<Data> responseList = new ArrayList<>();
+		
+		Map<String, BigDecimal> tenantWiseAmountCollectionSorted = tenantWiseAmountCollection.entrySet().parallelStream()
+        .sorted(Map.Entry.comparingByValue())
+        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue,
+                        (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+		
+		tenantWiseAmountCollectionSorted.forEach((key,value) -> {
+			List<Plot> plots = Arrays.asList(Plot.builder().name(key).value(value).build());
+			responseList.add(Data.builder().plots(plots).build());
+		});
+		
+		return responseList;
+		
 	}
 
 	public List<Data> collectionByUsageType(ChartCriteria chartCriteria) {
---
+++ dashboard-services/src/main/java/org/egov/dss/util/DashboardUtils.java
@@ -0,0 +1,30 @@
+package org.egov.dss.util;
+
+import java.util.Calendar;
+import java.util.TimeZone;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DashboardUtils {
+	
+	public Long getStartingTime(Long fromDate) {
+		Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+		cal.setTimeInMillis(fromDate);
+		cal.set(Calendar.HOUR, 0);
+		cal.set(Calendar.MINUTE, 0);
+		cal.set(Calendar.SECOND, 1);
+		return cal.getTimeInMillis();
+	}
+	
+	public Long getEndingTime(Long fromDate) {
+		Calendar cal = Calendar.getInstance(TimeZone.getTimeZone("GMT"));
+		cal.setTimeInMillis(fromDate);
+		cal.set(Calendar.HOUR, 11);
+		cal.set(Calendar.MINUTE, 59);
+		cal.set(Calendar.SECOND, 59);
+		cal.set( Calendar.AM_PM, Calendar.PM);
+		return cal.getTimeInMillis();
+	}
+
+}
