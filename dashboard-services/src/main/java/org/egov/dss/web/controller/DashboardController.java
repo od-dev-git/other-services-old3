e4d9551053d2bdf895b2adf25b996f20ae1198a8
Prasun Naiya <prasun.naiya@pwc.com>
2023-03-17 18:48:30 +0530

    api naming convention change

--- dashboard-services/src/main/java/org/egov/dss/web/controller/DashboardController.java
+++ dashboard-services/src/main/java/org/egov/dss/web/controller/DashboardController.java
@@ -34,7 +34,7 @@ public class DashboardController {
 
 	}
 	
-	@PostMapping("/startScheduler")
+	@PostMapping("/_generateData")
 	public ResponseEntity<DssResponse> startScheduler(@RequestBody @Valid final RequestInfoWrapper requestInfoWrapper) {
 		service.processRequest(requestInfoWrapper);
 		DssResponse response = DssResponse.builder()
