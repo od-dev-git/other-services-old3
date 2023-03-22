128b778881683c41741a5348b020f18dcc5a92f2
Chhayanka123 <chhayanka.sahu@pwc.com>
2023-03-22 10:03:46 +0530

    Modify collection query for dashboard

--- dashboard-services/src/main/java/org/egov/dss/constants/DashboardConstants.java
+++ dashboard-services/src/main/java/org/egov/dss/constants/DashboardConstants.java
@@ -71,6 +71,8 @@ public class DashboardConstants {
 	public static final String MODULE_LEVEL_PGR = "PGR";  
 	
 	public static final String STATUS_APPROVED = "APPROVED";
+	
+	public static final String STATUS_CANCELLED = "CANCELLED";
 
 	public static final List<String> OBPS_REVENUE_ALL_BS = Collections
 			.unmodifiableList(Arrays.asList(BUSINESS_SERVICE_BPA_APP_FEE, BUSINESS_SERVICE_BPA_SAN_FEE));
