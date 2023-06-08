c16f5a01e73b661063fb2f297b44da08789ba057
Chhayanka123 <chhayanka.sahu@pwc.com>
2023-06-08 19:59:36 +0530

    Demand update scheduler trigger time change

--- dashboard-services/src/main/java/org/egov/dss/web/controller/DashboardController.java
+++ dashboard-services/src/main/java/org/egov/dss/web/controller/DashboardController.java
@@ -79,7 +79,7 @@ public class DashboardController {
 		
 	}
 	
-	@Scheduled(cron = "0 0 * * SAT")
+	@Scheduled(cron = "0 0 * * SUN")
 	public void triggerDemandScheduler() {
 		log.info("Demand data update scheduler started..");
 		RequestInfoWrapper requestInfoWrapper = new RequestInfoWrapper();
