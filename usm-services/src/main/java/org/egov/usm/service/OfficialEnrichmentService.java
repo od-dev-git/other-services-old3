f1402aff9daa65e2fd096b6ccb9b4d867dfe37c9
jdash006 <jayaprakash.dash@pwc.com>
2023-08-18 10:55:12 +0530

    official reassign and deassign api added

--- usm-services/src/main/java/org/egov/usm/service/OfficialEnrichmentService.java
+++ usm-services/src/main/java/org/egov/usm/service/OfficialEnrichmentService.java
@@ -7,6 +7,7 @@ import org.egov.common.contract.request.RequestInfo;
 import org.egov.tracer.model.CustomException;
 import org.egov.usm.utility.USMUtil;
 import org.egov.usm.web.model.AuditDetails;
+import org.egov.usm.web.model.USMOfficial;
 import org.egov.usm.web.model.USMOfficialRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -44,5 +45,47 @@ public class OfficialEnrichmentService {
 		usmOfficialRequest.getUsmOffcial().setCategory(usmOfficialRequest.getUsmOffcial().getCategory());
 		usmOfficialRequest.getUsmOffcial().setRole(usmOfficialRequest.getUsmOffcial().getRole());
 		usmOfficialRequest.getUsmOffcial().setAuditDetails(auditDetails);
+	}
+
+	public void enrichDeassignOfficialRequest(@Valid USMOfficialRequest usmOfficialRequest,
+			USMOfficial existingOfficialMember) {
+		RequestInfo requestInfo = usmOfficialRequest.getRequestInfo();
+		AuditDetails auditDetails = USMUtil.getAuditDetails(requestInfo.getUserInfo().getUuid(), false);
+		usmOfficialRequest.getUsmOffcial().setAssigned(null);
+		usmOfficialRequest.getUsmOffcial().setTenantId(existingOfficialMember.getTenantId());
+		usmOfficialRequest.getUsmOffcial().setWard(existingOfficialMember.getWard());
+		usmOfficialRequest.getUsmOffcial().setCategory(existingOfficialMember.getCategory());
+		usmOfficialRequest.getUsmOffcial().setRole(existingOfficialMember.getRole());
+		usmOfficialRequest.getUsmOffcial().setSlumcode(existingOfficialMember.getSlumcode());
+		usmOfficialRequest.getUsmOffcial().setAuditDetails(auditDetails);
+
+	}
+
+	public void enrichReassignMembersRequest(@Valid USMOfficialRequest usmOfficialRequest,
+			USMOfficial existingOfficial) {
+		RequestInfo requestInfo = usmOfficialRequest.getRequestInfo();
+		AuditDetails auditDetails = USMUtil.getAuditDetails(requestInfo.getUserInfo().getUuid(), false);
+		auditDetails.setCreatedBy(existingOfficial.getAuditDetails().getCreatedBy());
+		auditDetails.setCreatedTime(existingOfficial.getAuditDetails().getCreatedTime());
+
+		String officialAccountId = null;
+
+		// Check User isUserPresent as Citizen
+		officialAccountId = userService.isEmployeePresent(usmOfficialRequest.getUsmOffcial().getAssigned(), requestInfo,
+				usmOfficialRequest.getUsmOffcial().getTenantId());
+
+		// If not present throw exception
+		if (StringUtils.isEmpty(officialAccountId)) {
+			throw new CustomException("ID ERROR", "Official id is not present");
+		}
+
+		usmOfficialRequest.getUsmOffcial().setAssigned(officialAccountId);
+		usmOfficialRequest.getUsmOffcial().setTenantId(existingOfficial.getTenantId());
+		usmOfficialRequest.getUsmOffcial().setSlumcode(existingOfficial.getSlumcode());
+		usmOfficialRequest.getUsmOffcial().setCategory(existingOfficial.getCategory());
+		usmOfficialRequest.getUsmOffcial().setWard(existingOfficial.getWard());
+		usmOfficialRequest.getUsmOffcial().setRole(existingOfficial.getRole());
+		usmOfficialRequest.getUsmOffcial().setAuditDetails(auditDetails);
+
 	}
 }
