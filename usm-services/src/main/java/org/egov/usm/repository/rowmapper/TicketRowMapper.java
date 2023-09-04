7dcfa57dddd5b3f00897cc37350e5ab5f6ef8afb
jdash006 <jayaprakash.dash@pwc.com>
2023-09-04 10:36:56 +0530

    surveyTicketComment added in ticket

--- usm-services/src/main/java/org/egov/usm/repository/rowmapper/TicketRowMapper.java
+++ usm-services/src/main/java/org/egov/usm/repository/rowmapper/TicketRowMapper.java
@@ -3,6 +3,8 @@ package org.egov.usm.repository.rowmapper;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,9 +12,11 @@ import java.util.Map;
 import org.egov.usm.model.enums.TicketStatus;
 import org.egov.usm.web.model.AuditDetails;
 import org.egov.usm.web.model.SurveyTicket;
+import org.egov.usm.web.model.SurveyTicketComment;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.ObjectUtils;
 
 @Component
@@ -36,32 +40,60 @@ public class TicketRowMapper implements ResultSetExtractor<List<SurveyTicket>> {
 						.createdTime(rs.getLong("createdtime")).lastModifiedBy(rs.getString("lastmodifiedby"))
 						.lastModifiedTime(lastModifiedTime).build();
 
-				ticket = SurveyTicket.builder()
-							.id(rs.getString("id"))
-							.tenantId(rs.getString("tenantid"))
-							.ticketNo(rs.getString("ticketno"))
-							.surveyAnswerId(rs.getString("surveyanswerid"))
-							.questionId(rs.getString("questionid"))
-							.ticketDescription(rs.getString("ticketdescription"))
-							.status(TicketStatus.fromValue(rs.getString("status")))
-							.ticketCreatedTime(rs.getLong("ticketcreatedtime"))
-							.ticketClosedTime(rs.getLong("ticketclosedtime"))
-							.ward(rs.getString("ward"))
-							.slumCode(rs.getString("slumcode"))
-							.questionCategory(rs.getString("questioncategory"))
-							.auditDetails(auditdetails).build();
-				
+				ticket = SurveyTicket.builder().id(rs.getString("id")).tenantId(rs.getString("tenantid"))
+						.ticketNo(rs.getString("ticketno")).surveyAnswerId(rs.getString("surveyanswerid"))
+						.questionId(rs.getString("questionid")).ticketDescription(rs.getString("ticketdescription"))
+						.status(TicketStatus.fromValue(rs.getString("status")))
+						.ticketCreatedTime(rs.getLong("ticketcreatedtime"))
+						.ticketClosedTime(rs.getLong("ticketclosedtime")).ward(rs.getString("ward"))
+						.slumCode(rs.getString("slumcode")).questionCategory(rs.getString("questioncategory"))
+						.auditDetails(auditdetails).build();
+
 				if (!ObjectUtils.isEmpty(rs.getObject("issatisfied"))) {
 					ticket.setIsSatisfied(rs.getBoolean("issatisfied"));
 				}
-				
+
 				if (!ObjectUtils.isEmpty(rs.getObject("unattended"))) {
 					ticket.setUnAttended(rs.getBoolean("unattended"));
 				}
+			}
+			addCommentToTicket(rs, ticket);
+
+			List<SurveyTicketComment> surveyTicketComments = ticket.getSurveyTicketComments();
+			if (!CollectionUtils.isEmpty(surveyTicketComments)) {
+				Collections.sort(ticket.getSurveyTicketComments(), Comparator.comparing(SurveyTicketComment::getId));
 			}
 
 			surveyMap.put(id, ticket);
 		}
 		return new ArrayList<>(surveyMap.values());
+	}
+
+	private void addCommentToTicket(ResultSet rs, SurveyTicket ticket) throws SQLException {
+		String commentId = rs.getString("commentid");
+		String ticketId = rs.getString("ticketid");
+
+		if (commentId == null || ticketId == null) {
+			ticket.addTicketComment(null);
+			return;
+		}
+
+		List<SurveyTicketComment> surveyTicketComments = ticket.getSurveyTicketComments();
+
+		if (!CollectionUtils.isEmpty(surveyTicketComments))
+			for (SurveyTicketComment comment : surveyTicketComments) {
+				if (comment.getId().equals(commentId))
+					return;
+			}
+
+		AuditDetails auditdetails = AuditDetails.builder().createdBy(rs.getString("createdby"))
+				.createdTime(rs.getLong("createdtime")).lastModifiedBy(rs.getString("lastmodifiedby"))
+				.lastModifiedTime(rs.getLong("lastmodifiedtime")).build();
+
+		SurveyTicketComment surveyTicketComment = SurveyTicketComment.builder().id(commentId).ticketId(ticketId)
+				.comment("comment").auditDetails(auditdetails).build();
+
+		ticket.addTicketComment(surveyTicketComment);
+
 	}
 }
