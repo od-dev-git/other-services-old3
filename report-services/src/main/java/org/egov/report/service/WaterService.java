c3cd81382e561940bd2c99ae3ba43cd41affb64c
jdash006 <jayaprakash.dash@pwc.com>
2023-11-14 15:52:34 +0530

    date parametre added

--- report-services/src/main/java/org/egov/report/service/WaterService.java
+++ report-services/src/main/java/org/egov/report/service/WaterService.java
@@ -114,8 +114,8 @@ public class WaterService {
 		PaymentSearchCriteria paymentSearchCriteria = PaymentSearchCriteria.builder()
 				.businessServices(Stream.of("WS","WS.ONE_TIME_FEE").collect(Collectors.toSet()))
 				.tenantId(searchCriteria.getTenantId())
-				.fromDate(searchCriteria.getCollectionDate())
-				.toDate(searchCriteria.getCollectionDate())
+				.fromDate(searchCriteria.getFromDate())
+				.toDate(searchCriteria.getToDate())
 				.build();
 		log.info(" Payments Search Criteria : " + paymentSearchCriteria.toString());
 		
