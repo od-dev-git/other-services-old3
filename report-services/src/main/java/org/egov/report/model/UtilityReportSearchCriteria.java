1ce41eff8c89ca1db7fe557d22d7871703e37658
Smruti Ranjan Rout <smruti.ranjan.rout.tpr@pwc.com>
2024-01-19 18:39:08 +0530

    search criteria updated and temporary location updated.

--- report-services/src/main/java/org/egov/report/model/UtilityReportSearchCriteria.java
+++ report-services/src/main/java/org/egov/report/model/UtilityReportSearchCriteria.java
@@ -1,5 +1,6 @@
 package org.egov.report.model;
 
+import java.math.BigDecimal;
 import java.util.Set;
 
 import lombok.AllArgsConstructor;
@@ -16,6 +17,10 @@ public class UtilityReportSearchCriteria {
     private Set<String> ids;
 
     private String tenantId;
+    
+    private BigDecimal fromDate;
+    
+    private BigDecimal toDate;
 
     private String reportType;
 
