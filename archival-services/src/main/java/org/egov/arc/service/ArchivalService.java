9739d716fe78cc0c3a84ad546ddf0f1670ca7c43
Chhayanka123 <chhayanka.sahu@pwc.com>
2024-04-17 19:10:26 +0530

    Add partition for demand data archival

--- archival-services/src/main/java/org/egov/arc/service/ArchivalService.java
+++ archival-services/src/main/java/org/egov/arc/service/ArchivalService.java
@@ -1,9 +1,7 @@
 package org.egov.arc.service;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -13,19 +11,20 @@ import java.util.Map.Entry;
 import java.util.Set;
 import java.util.stream.Collectors;
 
-import org.apache.catalina.startup.ClassLoaderFactory.Repository;
+import javax.swing.event.TableColumnModelListener;
+
 import org.egov.arc.config.ArchivalConfig;
 import org.egov.arc.model.Demand;
 import org.egov.arc.model.DemandCriteria;
 import org.egov.arc.model.DemandDetail;
 import org.egov.arc.repository.ArchivalRepository;
+import org.egov.arc.util.ArchivalUtility;
 import org.egov.arc.util.Util;
 import org.egov.arc.util.Constants.Constants;
 import org.egov.arc.validator.ArchivalValidator;
 import org.egov.common.contract.request.RequestInfo;
 import org.javers.common.collections.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.concurrent.DefaultManagedTaskScheduler;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 import org.springframework.util.StringUtils;
@@ -62,6 +61,7 @@ public class ArchivalService {
 	//	Set<Demand> archivalDemands = getArchiveDemands(demandCriteria, requestInfo);
 		Set<Demand> archivalDemands = getArchiveDemandsV2(demandCriteria, requestInfo);		
 		Set<Demand> distinctDemand =  archivalDemands.stream().distinct().collect(Collectors.toSet());
+		log.info("Archival Demand Size : "+distinctDemand.size());
 		distinctDemand.forEach(d -> log.info("Demand Id for Archival: " + d));
 		log.info(String.valueOf(distinctDemand.size()));
 		archivalRepository.insertArchiveDemands(distinctDemand);
@@ -132,36 +132,6 @@ public class ArchivalService {
 				}
 				break;
 
-			case Constants.PT_MUTATION:
-				if (archivalConfig.getDemandPtArchival()) {
-					wsApplicationFeeDemandArchival(archivaTillDate, archiveDemands, demandByBS);
-				}
-				break;
-
-			case Constants.TL:
-				if (archivalConfig.getDemandTlArchival()) {
-					wsApplicationFeeDemandArchival(archivaTillDate, archiveDemands, demandByBS);
-				}
-				break;
-
-			case Constants.MR:
-				if (archivalConfig.getDemandMrArchival()) {
-					wsApplicationFeeDemandArchival(archivaTillDate, archiveDemands, demandByBS);
-				}
-				break;
-
-			case Constants.BPA_NC_APP_FEE:
-				if (archivalConfig.getDemandBpaArchival()) {
-					wsApplicationFeeDemandArchival(archivaTillDate, archiveDemands, demandByBS);
-				}
-				break;
-
-			case Constants.BPA_NC_SAN_FEE:
-				if (archivalConfig.getDemandBpaArchival()) {
-					wsApplicationFeeDemandArchival(archivaTillDate, archiveDemands, demandByBS);
-				}
-				break;
-
 			default:
 				break;
 			}
@@ -204,34 +174,25 @@ public class ArchivalService {
 	 * @param archiveDemands
 	 * @param demandByBS
 	 */
-	private void wsUsageFeeDemandArchival(Long archivaTillDate, Set<Demand> archiveDemands,
+	private void wsUsageFeeDemandArchival(Long archivalTillDate, Set<Demand> archiveDemands,
 			Entry<String, List<Demand>> demandByBS) {
-		// List<Demand> latestDemandTillArchival =
-		// getLatestDemandTillArchival(Sets.newHashSet(Constants.WS),archivaTillDate);
-
-		List<Demand> result = new ArrayList<>();
 		List<Demand> wsUsageDemand = demandByBS.getValue();
 
-		for (Demand demand : wsUsageDemand) {
-			if (demand.getIsPaymentCompleted()) {
-				if (demand.getTaxPeriodFrom() <= archivaTillDate) {
-					List<Demand> wsDemand = wsUsageDemand.stream()
-							.filter(d -> d.getConsumerCode().equals(demand.getConsumerCode()))
+		wsUsageDemand.stream()
+				.filter(demand -> demand.getIsPaymentCompleted() && demand.getTaxPeriodFrom() <= archivalTillDate)
+				.collect(Collectors.groupingBy(Demand::getConsumerCode)) // Group demands by consumer code
+				.forEach((consumerCode, demandsForConsumer) -> {
+					List<Demand> sortedDemands = demandsForConsumer.stream()
+							.sorted(Comparator.comparing(Demand::getTaxPeriodFrom).reversed())
 							.collect(Collectors.toList());
 
-					result = wsDemand.stream().sorted(Comparator.comparing(Demand::getTaxPeriodFrom).reversed()).skip(1)
-							.filter(dem -> dem.getIsPaymentCompleted() && dem.getTaxPeriodFrom() < archivaTillDate)
+					// Skip the first demand (latest) and filter demands before archivalTillDate
+					List<Demand> archivalDemands = sortedDemands.stream().skip(1)
+							.filter(demand -> demand.getTaxPeriodFrom() < archivalTillDate)
 							.collect(Collectors.toList());
 
-					for (Demand d : result) {
-						archiveDemands.add(d);
-					}
-
-				}
-
-			}
-			// else {}
-		}
+					archiveDemands.addAll(archivalDemands);
+				});
 	}
 
 	/**
@@ -259,8 +220,9 @@ public class ArchivalService {
 	 */
 	@SuppressWarnings("null")
 	public Set<Demand> getArchiveDemandsV2(DemandCriteria demandCriteria, RequestInfo requestInfo) {
+		String tenantObj = ArchivalUtility.getSystemProperties().getTenants();
+		List<Object> tenants = Arrays.asList(tenantObj.split("\\s+"));
 		enrichDemandCriteria(demandCriteria);
-		List<Demand> demands = new ArrayList<>();
 		Set<Demand> archiveDemands = new HashSet<>();
 		Long archivalTillDate = util.getArchivalMonthStartDate(demandCriteria.getArchiveTillMonth());
 		log.info("Archive till date : " + archivalTillDate);
@@ -268,69 +230,45 @@ public class ArchivalService {
 		Long count = archivalRepository.getDemandCount(demandCriteria);
 		log.info("Total Demand record count till archival date:" + count + " Business Service : "
 				+ demandCriteria.getBusinessServices());
-		demands = archivalRepository.getDemands(demandCriteria);
-		HashMap<String, Demand> demandMap = new HashMap<>();
-		if (!CollectionUtils.isEmpty(demands)) {
-			for (Demand d : demands) {
-				DemandDetail demandDetail = new DemandDetail();
-				demandMap.put(d.getId(), d);
-				for (DemandDetail dd : d.getDemandDetails()) {
-					if (dd.getDemandId().equals(d.getId()) && demandMap.keySet().contains(dd.getDemandId()))
-						d.setDemandDetails(Sets.newHashSet(dd));
+		for (Object tenant : tenants) {
+			demandCriteria.setTenantId(String.valueOf(tenant));
+			Long countForTenant = archivalRepository.getDemandCount(demandCriteria);
+			log.info("Total Demand record count till archival date:" + countForTenant + "for tenant :"
+					+ String.valueOf(tenant) + " Business Service : " + demandCriteria.getBusinessServices());
+			List<Demand> demands = archivalRepository.getDemands(demandCriteria);
+			if (!CollectionUtils.isEmpty(demands)) {
+				Map<String, List<Demand>> demandGroupByBS = demands.stream().flatMap(demand -> demand.getDemandDetails()
+						.stream().collect(Collectors.toMap(DemandDetail::getDemandId, dd -> demand, (d1, d2) -> {
+							d1.getDemandDetails().add(d2.getDemandDetails().iterator().next());
+							return d1;
+						})).values().stream()).collect(Collectors.groupingBy(Demand::getBusinessService));
+
+				log.info("Distinct Demands :" + demands.toString());
+
+				for (Entry<String, List<Demand>> demandByBS : demandGroupByBS.entrySet()) {
+					String businessService = demandByBS.getKey();
+					switch (businessService) {
+					case Constants.WS_ONE_TIME_FEE:
+						if (archivalConfig.getDemandWsArchival()) {
+							wsApplicationFeeDemandArchival(archivalTillDate, archiveDemands, demandByBS);
+						}
+						break;
+
+					case Constants.WS:
+						if (archivalConfig.getDemandWsArchival()) {
+							wsUsageFeeDemandArchival(archivalTillDate, archiveDemands, demandByBS);
+						}
+						break;
+
+					default:
+						break;
+					}
+
 				}
+			} else {
+				log.info("No Demand found for archival criteria...tenant : " + tenant);
 			}
 		}
-		List<Demand> distinctDemands = new ArrayList<>();
-		for (Entry<String, Demand> dm : demandMap.entrySet()) {
-			distinctDemands.add(dm.getValue());
-		}
-
-		log.info("Distinct Demands :" + distinctDemands.toString());
-		Map<String, List<Demand>> demandGroupByBS = ((Collection<Demand>) distinctDemands).stream()
-				.collect(Collectors.groupingBy(Demand::getBusinessService));
-
-		for (Entry<String, List<Demand>> demandByBS : demandGroupByBS.entrySet()) {
-			String businessService = demandByBS.getKey();
-			switch (businessService) {
-			case Constants.WS_ONE_TIME_FEE:
-				if (archivalConfig.getDemandWsArchival()) {
-					wsApplicationFeeDemandArchival(archivalTillDate, archiveDemands, demandByBS);
-				}
-				break;
-
-			case Constants.WS:
-				if (archivalConfig.getDemandWsArchival()) {
-					wsUsageFeeDemandArchival(archivalTillDate, archiveDemands, demandByBS);
-				}
-				break;
-			/*
-			 * case Constants.PT_MUTATION: if (archivalConfig.getDemandPtArchival()) {
-			 * wsApplicationFeeDemandArchival(archivaTillDate, archiveDemands, demandByBS);
-			 * } break;
-			 * 
-			 * case Constants.TL: if (archivalConfig.getDemandTlArchival()) {
-			 * wsApplicationFeeDemandArchival(archivaTillDate, archiveDemands, demandByBS);
-			 * } break;
-			 * 
-			 * case Constants.MR: if (archivalConfig.getDemandMrArchival()) {
-			 * wsApplicationFeeDemandArchival(archivaTillDate, archiveDemands, demandByBS);
-			 * } break;
-			 * 
-			 * case Constants.BPA_NC_APP_FEE: if (archivalConfig.getDemandBpaArchival()) {
-			 * wsApplicationFeeDemandArchival(archivaTillDate, archiveDemands, demandByBS);
-			 * } break;
-			 * 
-			 * case Constants.BPA_NC_SAN_FEE: if (archivalConfig.getDemandBpaArchival()) {
-			 * wsApplicationFeeDemandArchival(archivaTillDate, archiveDemands, demandByBS);
-			 * } break;
-			 */
-
-			default:
-				break;
-			}
-
-		}
-		log.info("List of Archival Demands : " + archiveDemands);
 		return archiveDemands;
 	}
 
