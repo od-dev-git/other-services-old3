82fd8d0cff3b9a9f9ab5a237406018e4427f5eed
vmodisauser12 <anik.a.naskar@pwc.com>
2024-06-25 10:27:28 +0530

    Go Swift Error Resolving

--- integration-services/src/main/java/org/egov/integration/repository/ServiceRepository.java
+++ integration-services/src/main/java/org/egov/integration/repository/ServiceRepository.java
@@ -1,14 +1,17 @@
 package org.egov.integration.repository;
 
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.http.HttpRequest.BodyPublishers;
-import java.net.http.HttpResponse;
 import java.util.Map;
 
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 import org.egov.tracer.model.ServiceCallException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -68,28 +71,51 @@ public class ServiceRepository {
 		return response;
 	}
 	
+//	public String fetchResultObject(StringBuilder uri, String request) {
+//
+//
+//		 HttpClient client = HttpClient.newHttpClient();
+//	        try {
+//				HttpRequest httpRequest = HttpRequest.newBuilder()
+//				        .uri(new URI(uri.toString()))
+//				        .header("Content-Type", "application/json")
+//				        .POST(BodyPublishers.ofString("\"" + request + "\""))
+//				        .build();
+//				HttpResponse<String> response = client.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+//				return response.body();
+//
+//			} catch (URISyntaxException e) {
+//				e.printStackTrace();
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			} catch (InterruptedException e) {
+//				e.printStackTrace();
+//			}
+//	        return null;
+//
+//	}
+
 	public String fetchResultObject(StringBuilder uri, String request) {
-		
-		
-		 HttpClient client = HttpClient.newHttpClient();
-	        try {
-				HttpRequest httpRequest = HttpRequest.newBuilder()
-				        .uri(new URI(uri.toString()))
-				        .header("Content-Type", "application/json")
-				        .POST(BodyPublishers.ofString("\"" + request + "\""))
-				        .build();
-				HttpResponse<String> response = client.send(httpRequest, HttpResponse.BodyHandlers.ofString());
-				return response.body();
-				
-			} catch (URISyntaxException e) {
-				e.printStackTrace();
+		CloseableHttpClient client = HttpClients.createDefault();
+		try {
+			HttpPost httpPost = new HttpPost(new URI(uri.toString()));
+			httpPost.setHeader("Content-Type", "application/json");
+			httpPost.setEntity(new StringEntity("\"" + request + "\""));
+
+			CloseableHttpResponse response = client.execute(httpPost);
+			String responseBody = EntityUtils.toString(response.getEntity());
+			response.close();
+			return responseBody;
+		} catch (URISyntaxException | IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				client.close();
 			} catch (IOException e) {
 				e.printStackTrace();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
 			}
-	        return null;
-
+		}
+		return null;
 	}
 
 
